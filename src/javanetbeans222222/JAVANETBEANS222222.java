c39d774fb973e852c939bfdb9a46f96aae2bedf9
luis sosa <lsosae1@miumg.edu.com>
2023-10-28 07:38:03 -0600

    segundo commit

--- src/javanetbeans222222/JAVANETBEANS222222.java
+++ src/javanetbeans222222/JAVANETBEANS222222.java
@@ -15,6 +15,7 @@ public class JAVANETBEANS222222 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.print("Hola mundo");
         // TODO code application logic here
     }
     
